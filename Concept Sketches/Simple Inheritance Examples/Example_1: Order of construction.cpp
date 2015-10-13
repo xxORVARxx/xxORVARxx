f095b200cb8d987ceff3bb68d0cc9df7a85e907b
xxORVARxx <xxorvarxx@gmail.com>
2015-10-13 13:57:56 +0200

    Add: Inheritance Example 2

--- Concept Sketches/Simple Inheritance Examples/Example_1: Order of construction.cpp
+++ Concept Sketches/Simple Inheritance Examples/Example_1: Order of construction.cpp
@@ -7,7 +7,7 @@ class my_A
 {
 public:
   my_A( int s_x ) : m_x(s_x) { std::cout <<"my_A's Constructor Called.\n"; }
-  ~my_A() { std::cout <<"my_A's Destructor Called.\n"; }
+  ~my_A() { std::cout <<"my_A's ~Destructor Called.\n"; }
 protected:
   int m_x;
 };
@@ -18,7 +18,7 @@ class my_B: public my_A
 {
 public:
   my_B( int s_x, double s_y ) : my_A( s_x ), m_y(s_y) { std::cout <<"my_B's Constructor Called.\n"; }
-  ~my_B() { std::cout <<"my_B's Destructor Called.\n"; }
+  ~my_B() { std::cout <<"my_B's ~Destructor Called.\n"; }
 protected:
   double m_y;
 };
@@ -29,7 +29,7 @@ class my_C: public my_B
 {
 public:
   my_C( int s_x, double s_y, char s_z ) : my_B( s_x, s_y ), m_z(s_z) { std::cout <<"my_C's Constructor Called.\n"; }
-  ~my_C() { std::cout <<"my_C's Destructor Called.\n"; }
+  ~my_C() { std::cout <<"my_C's ~Destructor Called.\n"; }
   void Print() { std::cout <<"\n x:"<< m_x <<"  y:"<< m_y <<"  z:"<< m_z <<"\n\n"; }
 protected:
   char m_z;
@@ -43,3 +43,18 @@ int main( int argc, char* args[] )
   the_c.Print();
   return 0;
 }
+
+
+
+// This is what will be printed:
+
+/* my_A's Constructor Called.
+ * my_B's Constructor Called.
+ * my_C's Constructor Called.
+ *
+ *  x:5  y:4.3  z:R
+ *
+ * my_C's ~Destructor Called.
+ * my_B's ~Destructor Called.
+ * my_A's ~Destructor Called.
+ */
---
+++ Concept Sketches/Simple Inheritance Examples/Example_2: Multiple Inheritance.cpp
@@ -0,0 +1,105 @@
+
+#include <iostream> 
+
+
+
+struct my_Top {
+  char m_top;
+};
+
+
+// Let's say that this is the Base, not 'my_Top'.
+class my_Base : public my_Top
+{
+public:
+  my_Base( int s_xAB ) : m_x(s_xAB) {
+    std::cout <<"my_Base's Constructor Called.\n"; 
+  }
+  ~my_Base() { std::cout <<"my_Base's Destructor Called.\n"; }
+protected:
+  int m_x;
+};
+ 
+
+
+class my_Middle_A : public my_Base
+{
+public:
+  my_Middle_A( int s_xA ) : my_Base( s_xA ) { 
+    std::cout <<"  my_Middle_A's Constructor Called.\n"; 
+  }
+  ~my_Middle_A() { std::cout <<"  my_Middle_A's ~Destructor Called.\n"; }
+};
+ 
+
+
+class my_Middle_B : public my_Base
+{
+public:
+  my_Middle_B( int s_xB ) : my_Base( s_xB ) { 
+    std::cout <<"  my_Middle_B's Constructor Called.\n"; 
+  }
+  ~my_Middle_B() { std::cout <<"  my_Middle_B's ~Destructor Called.\n"; }
+};
+
+
+
+class my_End : public my_Middle_A, public my_Middle_B
+{
+public:
+  my_End( int s_xA, int s_xB ) : my_Middle_A( s_xA ), my_Middle_B( s_xB ) {
+    std::cout <<"    my_End's Constructor Called.\n"; 
+  }
+  ~my_End() { std::cout <<"    my_End's ~Destructor Called.\n"; }
+public:
+  int Get_A_x() { return my_Middle_A::m_x; }
+  int Get_B_x() { return my_Middle_B::m_x; }
+};
+
+
+
+int main( int argc, char* args[] )
+{
+  my_End c_end ( 65, -66 );
+  c_end.my_Middle_A::m_top = 'T';
+
+  std::cout <<"\n Get A's x: " << c_end.Get_A_x() <<".\n";
+  std::cout <<  " Get B's x: " << c_end.Get_B_x() <<".\n";
+  std::cout <<  " The Top is '"<< c_end.my_Middle_A::m_top <<"'.\n\n";
+
+  return 0;
+}
+
+
+
+// This is what will be printed:
+
+/* my_Base's Constructor Called.
+ *   my_Middle_A's Constructor Called.
+ * my_Base's Constructor Called.
+ *   my_Middle_B's Constructor Called.
+ *     my_End's Constructor Called.
+ *
+ *  Get A's x: 65.
+ *  Get B's x: -66.
+ *  The Top is 'T'.
+ *
+ *     my_End's ~Destructor Called.
+ *   my_Middle_B's ~Destructor Called.
+ * my_Base's Destructor Called.
+ *   my_Middle_A's ~Destructor Called.
+ * my_Base's Destructor Called.
+ */
+
+
+
+// This is what the inheritance tree looks like:
+
+/*           my_Top
+ *              |
+ *           my_Base
+ *           /     \
+ *  my_Middle_A    my_Middle_B
+ *           \     /
+ *           my_End 
+ */
